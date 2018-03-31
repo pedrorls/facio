2f097e607f10e99695249332c012b886483a2112
Pedro Silva <pedro.rls93@gmail.com>
2018-03-30 22:03:43 -0300

    new_list view was adjusted to do model validation

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from django.urls import resolve
 
-from .views import home_page
-from .models import Item, List
+from ..views import home_page
+from ..models import Item, List
+
 
 class HomePageTests(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
@@ -46,7 +47,7 @@ class NewListTest(TestCase):
         correct_lst = List.objects.create()
 
         self.client.post(
-            f'/lists/{correct_lst.id}/add_item', 
+            f'/lists/{correct_lst.id}/add_item',
             data={'item_text': 'A new list item for an existing list'}
         )
 
@@ -54,6 +55,18 @@ class NewListTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item for an existing list')
         self.assertEqual(new_item.list, correct_lst)
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = 'You cannot have an empty list item'
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_aret_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
 
     def test_redirects_to_list_view(self):
         other_lst = List.objects.create()
@@ -63,33 +76,3 @@ class NewListTest(TestCase):
             data={'item_text': 'A new list item for an existing list'}
         )
         self.assertRedirects(response, f'/lists/{correct_lst.id}/')
-
-
-
-class ListAndItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'The second item'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'The second item')
-        self.assertEqual(second_saved_item.list, list_)
--- lists/views.py
+++ lists/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render, redirect
+from django.core.exceptions import ValidationError
 
 from .models import Item, List
+
 
 def home_page(request):
     return render(request, 'home.html')
 
+
 def new_list(request):
     lst = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=lst)
+    item = Item.objects.create(text=request.POST['item_text'], list=lst)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        lst.delete()
+        error = 'You cannot have an empty list item'
+        return render(request, 'home.html', {'error': error})
     return redirect(f'/lists/{lst.id}/')
+
 
 def view_list(request, list_id):
     lst = List.objects.get(id=list_id)
     return render(request, 'list.html', {'list': lst})
+
 
 def add_item(request, lst_id):
     lst = List.objects.get(id=lst_id)
