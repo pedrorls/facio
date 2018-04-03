dc1e0036852a2bc108d77ba7b6d4c76b94068b5c
Pedro Silva <pedro.rls93@gmail.com>
2018-04-03 10:46:27 -0300

    FT validation test was passing

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,6 +3,10 @@ from .base import FunctionalTest
 
 
 class ItemValidationTest(FunctionalTest):
+
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-danger')
+
     def test_cannot_add_empty_list_items(self):
         self.browser.get(self.live_server_url)
         self.insert_list_item()
@@ -38,6 +42,20 @@ class ItemValidationTest(FunctionalTest):
         self.insert_list_item('Buy wellies')
 
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-danger').text,
+            self.get_error_element().text,
             'You have already got this item in your list'
+        ))
+
+    def test_error_messages_are_cleared_on_input(self):
+        self.browser.get(self.live_server_url)
+        self.insert_list_item('Banter too thick')
+        self.wait_for_row_in_list_table('1: Banter too thick')
+        self.insert_list_item('Banter too thick')
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+        self.get_item_input_box().send_keys('a')
+
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
         ))
