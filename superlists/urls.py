8d4d66d20be81432e219b0c686a536493b7fc2a8
Pedro Silva <pedro.rls93@gmail.com>
2018-04-05 22:28:26 -0300

    urls for accounts apps was included

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,15 +1,3 @@
-"""superlists URL Configuration
-
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, re_path, include
 
@@ -21,5 +9,5 @@ urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'^$', views.home_page, name='home'),
     re_path(r'^lists/', include(lists_urls)),
-    re_path(r'^accounts/', include(accounts_urls))
+    re_path(r'^accounts/', include(accounts_urls)),
 ]
