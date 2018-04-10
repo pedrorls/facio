0bec614cb20309d51f53c0549b4b4207362cb565
Pedro Silva <pedro.rls93@gmail.com>
2018-04-10 11:26:37 -0300

    mocking external dependencies to send link to login with email and token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.views import logout
 from django.urls import re_path
 from .views import *
 
 urlpatterns = [
-    re_path(r'send_login_email$', send_login_email, name='send_login_email'),
-    re_path(r'login$', login, name='login'),
+    re_path(r'^send_login_email$', send_login_email, name='send_login_email'),
+    re_path(r'^login$', login, name='login'),
+    re_path(r'^logout$', logout, {'next_page': '/'}, name='logout',)
 ]
